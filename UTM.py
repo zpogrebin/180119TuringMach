8b6a2d5bee9021f6f762ed43dedf5d2872ba9747
Zev Pogrebin <zpogrebin@putneyschool.org>
2018-01-25 23:01:31 -0500

    continuing previous

--- UTM.py
+++ UTM.py
@@ -3,7 +3,7 @@ import numpy as nn
 def right():
 	global pointer
 	if tape[pointer+1]==tape[-0]:
-	nn.append(char)
+		nn.append(char)
 	return 0
 
 def left():
@@ -15,13 +15,16 @@ def write():
 def statecommit(tstate):
 	return tstate
 
+filename = "t.txt"
+
 #-----Obtaining Information
 print "\033[1m--Zev Pogrebin UTM v1.0 --\033[0m"
 tapein = raw_input("Enter tape conents: ") #Obtain tape contents
-prog = input("Enter program path: ") #Obtain program file
-print "\n\033[1m___________________________\033[0m"
+progin = raw_input("Enter program path: ") #Obtain program file
+print "\n\033[1m__________________________\033[0m"
 print "Evaluating ",  tapein
-print "With instruction set ", prog
+print "With instruction set ", progin
+print "\n\033[1m__________________________\033[0m"
 
 #-----Initializing the tape'
 tape = nn.empty((len(tapein))) #Initialize other
@@ -32,12 +35,13 @@ for i in range (0,len(tapein)):
 #-----Initializing the instruction set
 instset = nn.empty((2,5)) # Initialize instruction set
 #Column Values instate | input | output | outstate | move 
-open(prog, "r") 
-print read(file) #testing
+prog = open(progin)
+print prog.read() #testing
 
 #-----Initializing global variables
 pointer=0
 state=0
+char= "#" 
 
 instset = nn.empty((2,5)) # Initialize instruction set
 # instate | input | output | outstate | move 
@@ -45,7 +49,7 @@ tape = nn.empty((len(tapein))) #Initialize other
 
 for i in range (0,len(tapein)):
 	tape[i]=tapein[i]
-	i += 1
+	i += i
 
-print instset
-print tape
+#print instset
+#print tape
