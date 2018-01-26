d706276458fb65b938cac7bed7e0b83ed727001d
Zev Pogrebin <zpogrebin@putneyschool.org>
2018-01-26 16:33:23 -0500

    added full initialization

--- UTM.py
+++ UTM.py
@@ -1,50 +1,60 @@
 import numpy as nn
 
-def right():
-	global pointer
-	if tape[pointer+1]==tape[-0]:
-		nn.append(char)
+#-----tape object
+class Tape():
+	def __init__(self, tapestring):
+		self.pointer = 0
+		self.tapearray = []
+		for i in range (0,len(tapestring)):
+			if tapestring[i] == "0" or tapestring == "1" or tapestring == char:
+				self.tapearray.append(str(tapestring[i]))
+			else: 
+				self.tapearray.append(str(char))
+				print "[Additional characters were found, they were converted to '" + char + "']"
+	
+	def right():
+		print pointer
+		if tape[pointer+1]==tape[-0]:
+			nn.append(char)
+		return 0
 
-	return 0
+	def left():
+		return 0
 
-def left():
-	return 0
+	def write():
+		return 0
 
-def write():
-	return 0
-
-def statecommit(tstate):
-	return tstate
-
-filename = "t.txt"
+	def statecommit(tstate):
+		return tstate
 
 #-----Obtaining Information
-print "\033[1m--Zev Pogrebin UTM v1.0 --\033[0m"
-tapein = raw_input("Enter tape conents: ") #Obtain tape contents
-progin = raw_input("Enter program path: ") #Obtain program file
+print "\n\033[1m--Zev Pogrebin UTM v0.0 --\033[0m"
+tapein = "1010100101" ###Temp
+progin = "sub.txt"
+diagnostic = True
+#tapein = raw_input("Enter tape conents: ") #Obtain tape contents
+#progin = raw_input("Enter program path: ") #Obtain program file
+#diagnostic = input("Diagnostic prints?: ") #
 print "\n\033[1m__________________________\033[0m"
-print "Evaluating ",  tapein
-print "With instruction set ", progin
-print "\n\033[1m__________________________\033[0m"
-
-#-----Initializing the tape'
-tape = nn.empty((len(tapein))) #Initialize other
-for i in range (0,len(tapein)):
-	tape[i]=tapein[i]
-	i += 1 #Insert values
+print "Evaluating\033[1m",  tapein, "\033[0m"
+print "With instruction set\033[1m", progin, "\033[0m:\n"
 
 #-----Initializing the instruction set
-instset = nn.empty(2,5,dtype=str) # Initialize instruction set
+instset = [] # Initialize instruction set
 #Column Values instate | input | outstate | output | move 
-prog = open(progin)
-print prog.read() #testing 
-###CURSOR###
+with open(progin) as prog:
+	state = prog.readline().rstrip()
+	char = prog.readline().rstrip()
+	inst = prog.readline().rstrip()
+	while inst:
+		instset.append(inst.split(","))
+		inst = prog.readline().rstrip()
 
-#-----Initializing global variables
-pointer=0
-state=0
-char=prog.readline(4)
-print char
 
-#print instset
-#print tape
+#logic
+
+if diagnostic==True:
+	print state
+	print char
+	print instset
+	print "tape placeholder" ## fix
