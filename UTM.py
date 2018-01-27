5c675c6324a3e64dd28e9ca560fa4dbfad3e0829
Zev Pogrebin <zpogrebin@putneyschool.org>
2018-01-26 19:52:38 -0500

    Added Tape.gethead() and working on Tape.move()

--- UTM.py
+++ UTM.py
@@ -1,31 +1,28 @@
 import numpy as nn
 
-#-----tape object
+#-----Tape object
 class Tape():
 	def __init__(self, tapestring):
 		self.pointer = 0
 		self.tapearray = []
 		for i in range (0,len(tapestring)):
-			if tapestring[i] == "0" or tapestring == "1" or tapestring == char:
+			if tapestring[i] == "0" or tapestring[i] == "1" or tapestring[i] == char:
 				self.tapearray.append(str(tapestring[i]))
 			else: 
 				self.tapearray.append(str(char))
-				print "[Additional characters were found, they were converted to '" + char + "']"
-	
-	def right():
-		print pointer
-		if tape[pointer+1]==tape[-0]:
-			nn.append(char)
-		return 0
+				print "[A '"+ tapestring[i] + "' character was found. It was converted to '" + char + "']"
 
-	def left():
-		return 0
+	def gethead():
+		return self.tapearray[self.pointer]
 
 	def write():
 		return 0
+	
+	def move(direction):
+		if direction == "R":
+			if pointer == len:
+				tapearray.append(char)
 
-	def statecommit(tstate):
-		return tstate
 
 #-----Obtaining Information
 print "\n\033[1m--Zev Pogrebin UTM v0.0 --\033[0m"
@@ -34,10 +31,10 @@ progin = "sub.txt"
 diagnostic = True
 #tapein = raw_input("Enter tape conents: ") #Obtain tape contents
 #progin = raw_input("Enter program path: ") #Obtain program file
-#diagnostic = input("Diagnostic prints?: ") #
+#diagnostic = input("Diagnostic prints?: ") #Diagnostic information
 print "\n\033[1m__________________________\033[0m"
 print "Evaluating\033[1m",  tapein, "\033[0m"
-print "With instruction set\033[1m", progin, "\033[0m:\n"
+print "With instruction set\033[1m", progin, "\033[0m:"
 
 #-----Initializing the instruction set
 instset = [] # Initialize instruction set
@@ -50,11 +47,11 @@ with open(progin) as prog:
 		instset.append(inst.split(","))
 		inst = prog.readline().rstrip()
 
-
+tape = Tape(tapein)
 #logic
-
 if diagnostic==True:
+	print "\033[1m__________________________\nDiagnostic Information\033[0m"
 	print state
 	print char
 	print instset
-	print "tape placeholder" ## fix
+	print tape.pointer
