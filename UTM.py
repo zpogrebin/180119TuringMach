3f5f3d5e831a8e667abfd9d8dc1859911210b7b4
Zev Pogrebin <zpogrebin@putneyschool.org>
2018-01-26 21:40:53 -0500

    finished, troubleshooting

--- UTM.py
+++ UTM.py
@@ -1,4 +1,13 @@
 import numpy as nn
+cont=True
+#-----Instruction search based on criteria state and inp
+def instsearch(st, iv):
+	for i in range(0, len(instset)):
+		templine = instset[i]
+		ststr = templine[0]
+		if int(ststr[1])==st and templine[1]==iv:
+			return i
+	return -1
 
 #-----Tape object
 class Tape():
@@ -12,46 +21,66 @@ class Tape():
 				self.tapearray.append(str(char))
 				print "[A '"+ tapestring[i] + "' character was found. It was converted to '" + char + "']"
 
-	def gethead():
+	def gethead(self):
 		return self.tapearray[self.pointer]
 
-	def write():
-		return 0
+	def writehead(self, digit):
+		if digit == "0" or digit == "1" or digit == char:
+			self.tapearray[self.pointer] = digit
+		else:
+			"[A '"+ digit + "' was in the instruction write. It was converted to '" + char + "']"
+			self.tapearray[self.pointer] = char
 	
-	def move(direction):
+	def move(self, direction):
+		print self.pointer
 		if direction == "R":
-			if pointer == len:
-				tapearray.append(char)
-
+			if self.pointer == len(self.tapearray)-1:
+				self.tapearray.append(char)
+			self.pointer = self.pointer + 1
+		if direction == "L":
+			if self.pointer == 0:
+				self.tapearray.insert(0, char)
+			else:
+				self.pointer = self.pointer - 1
+		print self.pointer
+		return self.pointer
 
 #-----Obtaining Information
 print "\n\033[1m--Zev Pogrebin UTM v0.0 --\033[0m"
-tapein = "1010100101" ###Temp
 progin = "sub.txt"
-diagnostic = True
-#tapein = raw_input("Enter tape conents: ") #Obtain tape contents
+diagnostic = False
+tapein = raw_input("Enter tape conents: ") #Obtain tape contents
 #progin = raw_input("Enter program path: ") #Obtain program file
 #diagnostic = input("Diagnostic prints?: ") #Diagnostic information
 print "\n\033[1m__________________________\033[0m"
 print "Evaluating\033[1m",  tapein, "\033[0m"
 print "With instruction set\033[1m", progin, "\033[0m:"
 
-#-----Initializing the instruction set
+
+#-----Initializing the instruction set and tape
 instset = [] # Initialize instruction set
 #Column Values instate | input | outstate | output | move 
 with open(progin) as prog:
-	state = prog.readline().rstrip()
+	state = int(prog.readline().rstrip()[1])
 	char = prog.readline().rstrip()
 	inst = prog.readline().rstrip()
 	while inst:
 		instset.append(inst.split(","))
 		inst = prog.readline().rstrip()
-
 tape = Tape(tapein)
-#logic
-if diagnostic==True:
-	print "\033[1m__________________________\nDiagnostic Information\033[0m"
-	print state
-	print char
-	print instset
-	print tape.pointer
+
+#-----Logic
+while cont==True:
+	instline = instsearch(state, tape.gethead())
+	if instline == -1:
+		break
+	tape.writehead(instset[instline][3])
+	print instset[instline]
+	tape.pointer = tape.move(instset[instline][4])
+	print tape.pointer,
+	state = int(instset[instline][2][1])
+	print state, "".join(tape.tapearray)
+
+#-----Ending
+print "Initd tape:", tapein
+print "Final tape:", "".join(tape.tapearray)
